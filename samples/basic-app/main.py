00036fef488c6fd1f5b4d20ed61740aa9ca0fad6
Adib Grouz <contact@adib-grouz.com>
2025-01-16 04:03:07 +0100

    intercepting request body, headers, prams ...

--- samples/basic-app/main.py
+++ samples/basic-app/main.py
@@ -1,19 +1,31 @@
-from storm.common.decorators.http import Get
+from storm.common.decorators.http import Get, Post
 from storm.common.decorators.module import Module
 from storm.common.decorators.controller import Controller
+from storm.common.services.logger import Logger
 from storm.core.application import StormApplication
 
 # Define Controller
 @Controller("/users")  # Define base path for this controller
 class UsersController():
+    
+    def __init__(self):
+        self.logger = Logger(self.__class__.__name__)
 
     @Get("/")
-    async def get_users(self, request):
+    async def get_users(self):
         return {"users": ["John", "Jane"]}
 
     @Get("/me")
-    async def get_me(self, request):
+    async def get_me(self):
         return {"users": ["John", "Jane"]}
+
+    @Post()
+    async def create_user(self):
+        return {"users": ["John", "Jane"]}
+
+    @Post("/stats")
+    async def get_users_stats(self):
+        return 1 + 1
 
 # Define Module
 @Module(controllers=[UsersController], imports=[])
