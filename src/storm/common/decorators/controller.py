bcb0357d7dcfda5f548ee80e2c5e2710cb06ed2d
Adi3g <contact@adib-grouz.com>
2025-01-16 19:33:21 +0100

    add inspector to the Controller decorator

--- src/storm/common/decorators/controller.py
+++ src/storm/common/decorators/controller.py
@@ -1,29 +1,45 @@
 from storm.core.controller import ControllerBase
+import inspect
 
 
 def Controller(base_path, middleware=[]):
     """
     A decorator for registering a controller and its routes.
-    
+
     :param base_path: The base path for the controller's routes.
     :param middleware: Optional middleware to be applied to the controller's routes.
     """
     def decorator(cls):
         # Modify the class initialization to handle ControllerBase initialization
         original_init = cls.__init__
+        # Access the function's signature
+        signature = inspect.signature(original_init)
 
         def new_init(self, *args, **kwargs):
             # Initialize ControllerBase automatically
             ControllerBase.__init__(self, base_path, middleware)
 
             # Call the original class's __init__, if needed (in case controller has custom logic)
-            original_init(self, *args, **kwargs)
+
+            # Prepare arguments for the original __init__ method
+            bound_args = signature.bind_partial(*args, **kwargs)
+            for name, param in signature.parameters.items():
+                if name not in bound_args.arguments:
+                    # Use the annotation if callable, otherwise default value
+                    if param.annotation is not inspect.Parameter.empty and callable(param.annotation):
+                        bound_args.arguments[name] = param.annotation()
+                    elif param.default is not inspect.Parameter.empty:
+                        bound_args.arguments[name] = param.default
+
+            # Call the original class's __init__ with prepared arguments
+            original_init(self, *bound_args.args, **bound_args.kwargs)
 
             # Automatically register routes
             for attr_name in dir(self):
                 attr = getattr(self, attr_name)
                 if callable(attr) and hasattr(attr, '_route'):
-                    self.routes.add_route(attr.route_method, base_path + attr.route_path, attr)
+                    self.routes.add_route(
+                        attr.route_method, base_path + attr.route_path, attr)
 
         cls.__init__ = new_init  # Replace the constructor with the new init
         return cls
