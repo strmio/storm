19fbeaa3d6804913ceba629216b015c3468d02ff
Adib Grouz <contact@adib-grouz.com>
2025-01-16 21:39:21 +0100

    refactor http decorators

--- src/storm/common/decorators/http.py
+++ src/storm/common/decorators/http.py
@@ -1,44 +1,47 @@
+from functools import wraps
+
+def route(method, path=""):
+    """Generic decorator to define HTTP routes."""
+    
+    def decorator(func):
+        # Use wraps to preserve function's metadata
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        
+        # Attach route metadata as attributes
+        wrapper.is_route = True
+        wrapper._route = {"method": method.upper(), "path": path}
+        wrapper.route_method = method.upper()
+        wrapper.route_path = path
+        wrapper.request = None
+
+        # Add a method to update the request attribute
+        def set_request(value):
+            wrapper.request = value
+        
+        wrapper.set_request = set_request
+        
+        return wrapper
+    
+    return decorator
 
 def Get(path=""):
     """Decorator to define a GET route."""
-    def decorator(func):
-        func.is_route = True
-        func._route = {"method": "GET", "path": path}  # Attach route metadata
-        func.route_method = "GET"
-        func.route_path = path  # Attach route path
-        return func
-    return decorator
-
+    return route("GET", path)
 
 def Post(path=""):
     """Decorator to define a POST route."""
-    def decorator(func):
-        func.is_route = True
-        func._route = {"method": "POST", "path": path}  # Attach route metadata
-        func.route_method = "POST"
-        func.route_path = path  # Attach route path
-        return func
-    return decorator
-
+    return route("POST", path)
 
 def Put(path=""):
-    """Decorator to define a POST route."""
-    def decorator(func):
-        func.is_route = True
-        func._route = {"method": "PUT", "path": path}  # Attach route metadata
-        func.route_method = "PUT"
-        func.route_path = path  # Attach route path
-        return func
-    return decorator
-
+    """Decorator to define a PUT route."""
+    return route("PUT", path)
 
 def Delete(path=""):
-    """Decorator to define a POST route."""
-    def decorator(func):
-        func.is_route = True
-        # Attach route metadata
-        func._route = {"method": "DELETE", "path": path}
-        func.route_method = "DELETE"
-        func.route_path = path  # Attach route path
-        return func
-    return decorator
+    """Decorator to define a DELETE route."""
+    return route("DELETE", path)
+
+def Patch(path=""):
+    """Decorator to define a PATCH route."""
+    return route("PATCH", path)
