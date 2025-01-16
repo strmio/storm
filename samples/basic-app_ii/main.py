b84fe1f884dc3808a46778088393ea645328790b
Adi3g <contact@adib-grouz.com>
2025-01-16 19:30:13 +0100

    add basic sample ii with service example

---
+++ samples/basic-app_ii/main.py
@@ -0,0 +1,52 @@
+from storm.common.decorators.http import Get
+from storm.common.decorators.injectable import Injectable
+from storm.common.decorators.module import Module
+from storm.common.decorators.controller import Controller
+from storm.common.services.logger import Logger
+from storm.core.application import StormApplication
+
+
+@Injectable()
+class UsersService():
+    def __init__(self):
+        self.logger = Logger(self.__class__.__name__)
+    
+    def get_users(self):
+        return {"users": ["John", "Jane"]}
+
+    def get_count(self):
+        return {"users_count": 1000 + 1}
+
+# Define Controller
+@Controller("/users")  # Define base path for this controller
+class UsersController():
+    
+    def __init__(self, users_service: UsersService):
+        self.logger = Logger(self.__class__.__name__)
+        self.users_service = users_service
+
+    @Get("/")
+    async def get_users(self):
+        return self.users_service.get_users()
+
+    @Get("/count")
+    async def get_users_count(self):
+        return self.users_service.get_count()
+
+
+# Define Module
+@Module(controllers=[UsersController], providers=[UsersService])
+class UsersModule:
+    pass
+
+@Module(imports=[UsersModule])
+class AppModule:
+    pass
+
+# Create the Storm Application and Run the Server
+if __name__ == "__main__":
+    # Initialize the application with AppModule
+    app = StormApplication(AppModule)
+
+    # Start the application
+    app.run()
