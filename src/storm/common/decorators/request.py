6acb4383892d626ecd581f29641e0c08ea4a00e9
Adi3g <contact@adib-grouz.com>
2025-01-17 21:47:52 +0100

    update Request & Req decorator

--- src/storm/common/decorators/request.py
+++ src/storm/common/decorators/request.py
@@ -1,16 +1,30 @@
 from functools import wraps
+from storm.common.execution_context import execution_context
 
 def Request(param_name):
-    """Decorator to pass the request data as a parameter to the function."""
+    """
+    Decorator to inject the current request from the execution context
+    as a parameter to the route handler.
+
+    :param param_name: The name of the parameter to inject the request into.
+    """
     def decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Check if the function has a `request` attribute
-            if hasattr(func, "request"):
-                # Inject the `request` data into kwargs
-                kwargs[param_name] = func.request
-            return func(*args, **kwargs)
+        async def wrapper(*args, **kwargs):
+            # Get the current request from the execution context
+            context = execution_context.get()
+            request = context.get("request", None)
+
+            # Inject the request into the kwargs using the given param_name
+            if request:
+                kwargs[param_name] = request
+
+            # Call the original function with the updated kwargs
+            return await func(*args, **kwargs)
+
         return wrapper
+
     return decorator
 
+# Alias for Request to allow it to be imported as Req or Request
 Req = Request
