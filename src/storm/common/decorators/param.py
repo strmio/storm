d00466785ef00af5d8662a30c65b636bea4c7634
Adib Grouz <contact@adib-grouz.com>
2025-01-18 18:36:16 +0100

    refactor Param to one single class

--- src/storm/common/decorators/param.py
+++ src/storm/common/decorators/param.py
@@ -1,37 +1,12 @@
 from functools import wraps
 from storm.common.execution_context import execution_context
 
-def Param(param_name = "params"):
+class Param:
     """
-    Decorator to inject a specific route parameter into the handler.
+    A unified implementation of Param/Params that can work both as a decorator
+    and as a dynamic parameter resolver in function arguments.
 
-    :param param_name: The name of the route parameter to inject.
-    """
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            # Get the current request from the execution context
-            context = execution_context.get()
-            route_params = context.get("request", {}).get("params", {})
-
-            # Inject the route parameter into kwargs if it exists
-            if param_name in route_params:
-                kwargs[param_name] = route_params[param_name]
-            else:
-                kwargs[param_name] = route_params
-
-            # Call the original function with updated kwargs
-            return await func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-class Params:
-    """
-    A placeholder for route parameters to be resolved dynamically.
-    If param_name is None, all route parameters will be returned.
+    :param param_name: The name of the route parameter to inject or resolve.
     """
     def __init__(self, param_name=None):
         self.param_name = param_name
@@ -46,3 +21,26 @@ class Params:
         if self.param_name is None:
             return route_params
         return route_params.get(self.param_name)
+
+    def __call__(self, func=None):
+        # If called without a function, act as a resolver
+        if func is None:
+            return self.resolve()
+
+        # If called with a function, act as a decorator
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            # Get the current request from the execution context
+            context = execution_context.get()
+            route_params = context.get("request", {}).get("params", {})
+
+            # Inject the route parameter into kwargs if it exists
+            if self.param_name in route_params:
+                kwargs[self.param_name] = route_params[self.param_name]
+            else:
+                kwargs[self.param_name] = route_params
+
+            # Call the original function with updated kwargs
+            return await func(*args, **kwargs)
+
+        return wrapper
