92de06ce05857fd7e180655300921760bc2e1440
Adib Grouz <contact@adib-grouz.com>
2025-01-19 18:15:21 +0100

    implement OnModuleInit, OnApplicationBootstrap, OnApplicationShutdown hooks

--- src/storm/core/hooks/hooks.py
+++ src/storm/core/hooks/hooks.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Lifecycle:
     def on_init(self):
         """
@@ -9,4 +11,22 @@ class Lifecycle:
         """
         Called when the service or controller is shutting down.
         """
+        pass
+
+class OnModuleInit(ABC):
+    @abstractmethod
+    def on_module_init(self):
+        """Called after the module has been initialized."""
+        pass
+
+class OnApplicationBootstrap(ABC):
+    @abstractmethod
+    def on_application_bootstrap(self):
+        """Called after the application has fully bootstrapped."""
+        pass
+
+class OnApplicationShutdown(ABC):
+    @abstractmethod
+    def on_application_shutdown(self, signal: str):
+        """Called during application shutdown."""
         pass
