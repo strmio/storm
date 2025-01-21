cb21730d90d83242f24b2a1e6625d2ab300cf0f2
Adib Grouz <contact@adib-grouz.com>
2025-01-21 15:29:08 +0100

    update project structure - separate modules

--- samples/hooks-app/controllers/notes_controllers.py
+++ samples/hooks-app/controllers/notes_controllers.py
@@ -4,7 +4,7 @@ from storm.common import Param
 from storm.common import ParseIntPipe
 from storm.common import Logger
 from services.users_notes_service import UsersNotesService
-from services.user_service import UsersService
+from services.users_service import UsersService
 
 @Controller("/users/:user_id/notes")  # Define base path for this controller
 class NotesController():
@@ -17,6 +17,8 @@ class NotesController():
     @Get("/:id")
     async def get_note(self, user_id: int = Param("user_id", ParseIntPipe),
                        id=Param("id", ParseIntPipe)):
+        user = self.user_service.get_user(user_id)
+        self.logger.info(f"Fetching note with ID: {id} for user: {user}")
         return self.notes_service.note_by_id(user_id, id)
     
     @Get()
--- samples/hooks-app/main.py
+++ samples/hooks-app/main.py
@@ -1,23 +1,7 @@
-from storm.common import Module
 from storm.core import StormApplication
-from services.user_service import UsersService
-from services.users_notes_service import UsersNotesService
-from controllers.users_controllers import UsersController
-from controllers.notes_controllers import NotesController
 
-@Module(controllers=[UsersController], providers=[UsersService])
-class UsersModule:
-    pass
+from modules.app_module import AppModule
 
-
-@Module(controllers=[NotesController], providers=[UsersNotesService, UsersService])
-class NotesModule:
-    pass
-
-
-@Module(imports=[UsersModule, NotesModule])
-class AppModule:
-    pass
 
 # Initialize the application with AppModule
 app = StormApplication(AppModule)
