263703653d56a4f2ae59bf2635485dea6731d863
Adib Grouz <contact@adib-grouz.com>
2025-01-28 21:47:41 +0100

    add type annotations

--- storm/common/interceptors/interceptor.py
+++ storm/common/interceptors/interceptor.py
@@ -1,13 +1,22 @@
+from typing import Callable, Awaitable, Any
+from storm.common.execution_context import ExecutionContext
+
+
 class Interceptor:
     """
-    Base class for all interceptors. Interceptors can transform requests before they reach the controller
-    and responses after the controller has processed the request.
+    Base class for all interceptors. Interceptors can transform the execution context before
+    it reaches the controller and transform the response after the controller processes the context.
     """
 
-    async def intercept(self, request, next):
+    async def intercept(
+        self,
+        context: ExecutionContext,
+        next: Callable[[ExecutionContext], Awaitable[Any]],
+    ) -> Any:
         """
-        Process the request and optionally transform the response.
-        :param request: The incoming request object.
+        Process the execution context and optionally transform the response.
+
+        :param context: The execution context for the current request.
         :param next: A function to call the next interceptor or controller action.
         :return: The response after processing.
         """
