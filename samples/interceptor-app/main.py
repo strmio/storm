c373dc29ec485ab7aa9854ab1e403cb4081a288e
Adib Grouz <contact@adib-grouz.com>
2025-01-28 20:49:29 +0100

    add interceptor sample app

---
+++ samples/interceptor-app/main.py
@@ -0,0 +1,46 @@
+from storm.core import StormApplication
+from storm.common import Interceptor, Logger
+from modules.app_module import AppModule
+
+
+# Initialize the application with AppModule
+app = StormApplication(AppModule)
+
+
+class GlobalLogInterceptor(Interceptor):
+    def __init__(self):
+        self.logger = Logger(self.__class__.__name__)
+
+    async def intercept(self, request, next):
+        """
+        Process the request and optionally transform the response.
+        :param request: The incoming request object.
+        :param next: A function to call the next interceptor or controller action.
+        :return: The response after processing.
+        """
+        self.logger.info("Logging Request")
+        return await next(request)
+
+
+class GlobalReqInterceptor(Interceptor):
+    def __init__(self):
+        self.logger = Logger(self.__class__.__name__)
+
+    async def intercept(self, request, next):
+        """
+        Process the request and optionally transform the response.
+        :param request: The incoming request object.
+        :param next: A function to call the next interceptor or controller action.
+        :return: The response after processing.
+        """
+        self.logger.info(f"Request: {request}")
+        return await next(request)
+
+
+app.add_global_interceptor(GlobalLogInterceptor)
+# app.add_global_interceptor(GlobalReqInterceptor)
+
+# Create the Storm Application and Run the Server
+if __name__ == "__main__":
+    # Start the application
+    app.run()
