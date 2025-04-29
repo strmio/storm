97fd755b38644535874e148a9d59803ab3a0d76b
Adi3g <contact@adib-grouz.com>
2025-04-29 21:28:40 +0200

    ft(common) - add sse metadata

--- storm/common/decorators/sse.py
+++ storm/common/decorators/sse.py
@@ -1,5 +1,8 @@
 from functools import wraps
 
+from storm.common.constants import SSE_METADATA
+from storm.core.reflector import Reflector
+
 
 def Sse(path=""):
     """Decorator to define an SSE (Server-Sent Events) GET route."""
@@ -15,6 +18,10 @@ def Sse(path=""):
         wrapper.route_method = "GET"
         wrapper.route_path = path
         wrapper.is_sse = True  # Mark it as an SSE route
+        Reflector.set_watermark(wrapper, SSE_METADATA)
+        Reflector.set_metadata(wrapper, "is_sse", True)
+        Reflector.set_metadata(wrapper, "path", path)
+        Reflector.set_metadata(wrapper, "method", "GET")
 
         return wrapper
 
