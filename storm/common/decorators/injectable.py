7a96ee69fff3d27e683d00a8bc0c01f62b8a1db6
Adi3g <contact@adib-grouz.com>
2025-04-29 21:27:31 +0200

    ft(common) - add injectable metadata

--- storm/common/decorators/injectable.py
+++ storm/common/decorators/injectable.py
@@ -1,3 +1,7 @@
+from storm.common.constants import INJECTABLE_WATERMARK
+from storm.core.reflector import Reflector
+
+
 def Injectable(singleton=True):
     """
     Mark a class as injectable.
@@ -5,6 +9,10 @@ def Injectable(singleton=True):
     """
 
     def wrapper(cls):
+        Reflector.set_watermark(cls, INJECTABLE_WATERMARK)
+        Reflector.set_metadata(cls, "__injectable__", True)
+        Reflector.set_metadata(cls, "__singleton__", singleton)
+        Reflector.set_metadata(cls, "class_name", cls.__name__)
         cls.__injectable__ = True
         cls.__singleton__ = singleton
         return cls
