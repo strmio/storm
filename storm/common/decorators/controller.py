54cc624471919fc7fc2e28fff78822248aa2dbed
Adi3g <contact@adib-grouz.com>
2025-04-29 21:27:12 +0200

    ft(common) - add controller metadata

--- storm/common/decorators/controller.py
+++ storm/common/decorators/controller.py
@@ -1,4 +1,8 @@
-def Controller(base_path="", middleware=[]):
+from storm.common.constants import CONTROLLER_WATERMARK, VERSION_METADATA
+from storm.core.reflector import Reflector
+
+
+def Controller(base_path="", middleware=[], version=None):
     """
     A decorator for registering a controller and its routes.
 
@@ -7,8 +11,13 @@ def Controller(base_path="", middleware=[]):
     """
 
     def decorator(cls):
+        Reflector.set_watermark(cls, CONTROLLER_WATERMARK)
+        Reflector.set_metadata(cls, "base_path", base_path)
+        Reflector.set_metadata(cls, "middleware", middleware)
+        Reflector.set_metadata(cls, VERSION_METADATA, version)
         cls.__base_path__ = base_path
         cls.__middleware__ = middleware
+        cls.__version__ = version
         return cls
 
     return decorator
