540eda218eca89cb46e0e8fc739ad44f5737899b
Adib Grouz <contact@adib-group.com>
2025-05-05 23:58:30 +0200

    fix(Core) - sse warapper & http response close

--- storm/core/wrappers/sse_wrapper.py
+++ storm/core/wrappers/sse_wrapper.py
@@ -2,6 +2,14 @@ from storm.common.execution_context import execution_context
 
 
 def _wrap_sse_handler(handler):
+    """
+    Wraps an SSE (Server-Sent Events) handler to adapt it for sending SSE events.
+
+    :param handler: An asynchronous function that returns an asynchronous generator.
+                    The generator yields data to be sent as SSE events.
+    :return: An asynchronous function that handles the SSE communication lifecycle,
+             including sending headers, streaming events, and closing the connection.
+    """
     async def sse_adapter(*args, **kwargs):
         request = execution_context.get_request()
         response = execution_context.get_response()
@@ -10,12 +18,10 @@ def _wrap_sse_handler(handler):
 
         try:
             generator = await handler()
-            async for event in generator:
+            async for data in generator:
                 await response.send_sse_event(
                     request.send,
-                    data=event.get("data"),
-                    event=event.get("event"),
-                    id=event.get("id"),
+                    data=data,
                 )
         finally:
             await response.close_sse(request.send)
