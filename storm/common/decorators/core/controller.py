310798a6a0a17572e49dc1a0bb8c207cd7468df0
Adib Grouz <contact@adib-group.com>
2025-05-07 01:58:59 +0200

    ft(Decorators) - add Version and use classe in @Injectable, @Controller

--- storm/common/decorators/core/controller.py
+++ storm/common/decorators/core/controller.py
@@ -2,7 +2,13 @@
 
 from typing import List, Optional, Type, Union
 
-from storm.common.constants import CONTROLLER_WATERMARK, HOST_METADATA, PATH_METADATA, SCOPE_OPTIONS_METADATA, VERSION_METADATA
+from storm.common.constants import (
+    CONTROLLER_WATERMARK,
+    HOST_METADATA,
+    PATH_METADATA,
+    SCOPE_OPTIONS_METADATA,
+    VERSION_METADATA,
+)
 from storm.common.interfaces.scope_options import Scope, ScopeOptions
 from storm.common.interfaces.version_options import VersionOptions, VersionValue
 from storm.common.utils.shared import deduplicate_preserve_order, is_string, is_undefined
@@ -24,40 +30,41 @@ class ControllerOptions(ScopeOptions, VersionOptions):
         self.host = host
 
 
-#
-def Controller(prefix_or_options: Union[str, List[str], ControllerOptions, None] = None):
+class Controller:
     """
-    Decorator to mark a class as a Storm controller.
+    Class decorator that marks a class as a Storm controller.
     """
-    default_path = "/"
 
-    if is_undefined(prefix_or_options):
-        path = default_path
-        host = None
-        scope_options = None
-        version = None
-    elif is_string(prefix_or_options) or isinstance(prefix_or_options, list):
-        path = prefix_or_options
-        host = None
-        scope_options = None
-        version = None
-    elif isinstance(prefix_or_options, ControllerOptions):
-        path = prefix_or_options.path or default_path
-        host = prefix_or_options.host
-        scope_options = {"scope": prefix_or_options.scope, "durable": prefix_or_options.durable}
+    def __init__(self, prefix_or_options: Union[str, List[str], ControllerOptions, None] = None):
+        self.default_path = "/"
+        self.path: Union[str, List[str]] = self.default_path
+        self.host: Optional[Union[str, List[str]]] = None
+        self.scope_options: Optional[dict] = None
+        self.version: Optional[VersionValue] = None
 
-        version = prefix_or_options.version
-        if isinstance(version, list):
-            version = deduplicate_preserve_order(version)
-    else:
-        raise TypeError("Invalid argument passed to @Controller decorator")
+        if is_undefined(prefix_or_options):
+            pass
+        elif is_string(prefix_or_options) or isinstance(prefix_or_options, list):
+            self.path = prefix_or_options
+        elif isinstance(prefix_or_options, ControllerOptions):
+            self.path = prefix_or_options.path or self.default_path
+            self.host = prefix_or_options.host
+            self.scope_options = {
+                "scope": prefix_or_options.scope,
+                "durable": prefix_or_options.durable,
+            }
 
-    def decorator(target: Type):
+            version = prefix_or_options.version
+            if isinstance(version, list):
+                version = deduplicate_preserve_order(version)
+            self.version = version
+        else:
+            raise TypeError("Invalid argument passed to @Controller decorator")
+
+    def __call__(self, target: Type) -> Type:
         Reflect.define_metadata(CONTROLLER_WATERMARK, True, target)
-        Reflect.define_metadata(PATH_METADATA, path, target)
-        Reflect.define_metadata(HOST_METADATA, host, target)
-        Reflect.define_metadata(SCOPE_OPTIONS_METADATA, scope_options, target)
-        Reflect.define_metadata(VERSION_METADATA, version, target)
+        Reflect.define_metadata(PATH_METADATA, self.path, target)
+        Reflect.define_metadata(HOST_METADATA, self.host, target)
+        Reflect.define_metadata(SCOPE_OPTIONS_METADATA, self.scope_options, target)
+        Reflect.define_metadata(VERSION_METADATA, self.version, target)
         return target
-
-    return decorator
